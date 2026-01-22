3382cf6046592e3062c8b037203283819929f996
just-yumi <81karumik@gmail.com>
2026-01-22 16:17:39 +0200

    Create session control method

--- CinemaWeb/CinemaWeb/Models/Session.cs
+++ CinemaWeb/CinemaWeb/Models/Session.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CinemaWeb.Models;
 
@@ -7,14 +8,19 @@ public partial class Session
 {
     public int Id { get; set; }
 
+    [Display(Name = "Назва фільму")] // Відображення тексту "Назва фільму" у формі, замість "Name"
     public int MovieId { get; set; }
 
+    [Display(Name = "Назва залу")]
     public byte HallId { get; set; }
 
+    [Display(Name = "Час початку сеансу")]
     public DateTime StartTime { get; set; }
 
+    [Display(Name = "Назва залу")]
     public virtual Hall Hall { get; set; } = null!;
 
+    [Display(Name = "Назва фільму")]
     public virtual Movie Movie { get; set; } = null!;
 
     public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
